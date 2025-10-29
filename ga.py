2313eaf9eef4375064b00ad8d14e4920095a28d9
Akmal-z <akmalzuhairi.az@gmail.com>
2025-10-29 18:13:19 +0800

    Change target string from 'fakhitah' to 'akmal'

--- ga.py
+++ ga.py
@@ -16,7 +16,7 @@ POP_SIZE = 500
 MUT_RATE = 0.2
 
 #TARGET: Our goal.
-TARGET = 'fakhitah'
+TARGET = 'akmal'
 
 #GENES: Options from which our population would be created.
 GENES = ' abcdefghijklmnopqrstuvwxyz'
